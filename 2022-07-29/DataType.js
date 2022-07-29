b1e1b7118dba774e7417737e0a82badde292bcc3
Sujin <ikosdu60@gmail.com>
2022-07-29 21:07:43 +0900

    [ADD]JS: 패스트캠퍼스 데이터타입

---
+++ 2022-07-29/DataType.js
@@ -0,0 +1,14 @@
+let whatever = 'Mark'; // 문자열 타입을 가지고 있는 변수
+
+whatever = 37; // whatever은 숫자형 타입이 됨.
+
+whatever = true // whatever은 boolean형 타입이 됨. 변수가 값에 따라 성질이 달라지게 되는것을 동적 타이핑이라고 한다.
+
+// Boolean
+
+const isTrue = true;
+const isFalse = false;
+
+console.log(isTrue, typeof isTrue);
+console.log(isFalse, typeof isFalse);
+
