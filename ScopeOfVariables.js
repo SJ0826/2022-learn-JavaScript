608dbb9bc173abb82cc94711009a57d5057e891f
SJ0826 <56298540+SJ0826@users.noreply.github.com>
2022-07-29 16:07:20 +0900

    [ADD]JS: 패스트캠퍼스 변수의 유효범위

---
+++ ScopeOfVariables.js
@@ -0,0 +1,18 @@
+//블럭
+{
+    const name = "Mark";
+    console.log(name);
+ }
+
+ // console. log(name)name; 블럭 밖에서 출력 코드를 작성하면 출력되지 않음.
+ 
+ // 밖에서 안으로는 사용 가능.
+
+ let age = 37;
+
+ {
+    age++;
+    console.log(age);
+ }
+
+ 
