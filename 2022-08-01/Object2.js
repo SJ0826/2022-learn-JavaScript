ac3db3f2e2c5e818602732323cc5c135c2025b07
Sujin <ikosdu60@gmail.com>
2022-08-01 16:05:26 +0900

    [ADD]JS: 패스트 캠퍼스 객체

---
+++ 2022-08-01/Object2.js
@@ -0,0 +1,13 @@
+// new Object
+
+const a = new Object();
+
+console.log(a, typeof a);
+
+const b = new Object(true);
+
+console.log(b, typeof b);
+
+const c = new Object({name : 'Mark'}); //리터럴 객체: 문자 그대로 작성한 객체
+
+console.log(c, typeof c);
