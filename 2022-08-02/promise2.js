b05516ac50b3b3a1370c418ec4871c665d898b7c
Sujin <ikosdu60@gmail.com>
2022-08-02 10:44:03 +0900

    [ADD]JS:패스트 캠퍼스 promise

--- 2022-08-02/promise2.js
+++ 2022-08-02/promise2.js
@@ -1,15 +1,12 @@
 /*
 p 라는 프로미스 객체는 1000ms 후에 fulfilled 됩니다.
-*/
-
-/*
 p 라는 프로미스 객체가 fulfilled 되는 시점에 p.then 안에 설정한 callback 함수가 실행됩니다.
 */
 
 const p = new Promise((resolve, reject) =>{
     /*pending*/
     setTimeout(() => { //setTimeout: 만료된 후 함수나 지정한 코드 조각을 실행하는 타이머를 설정합니다.
-        resolve(); //fuluilled
+        resolve(); //fuluille
     }, 1000 );
 });
 
