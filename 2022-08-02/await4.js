cbc33b650929f417571cf154a0c265fa7dc78477
Sujin <ikosdu60@gmail.com>
2022-08-02 21:24:25 +0900

    [ADD]JS: 패스트캠퍼스 async 함수 return

---
+++ 2022-08-02/await4.js
@@ -0,0 +1,23 @@
+//async function에서 return 되는 값은
+// Promise.resolve 함수로 감싸서 리턴된다.
+
+function p(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error('reason'));
+        }, ms)
+    })
+}
+
+async function asyncP() {
+    return 'Mark';
+}
+
+ (async function main() {
+    try{
+        const name = await asyncP();
+        console.log(name);
+    } catch (error) {
+        console.log(error);
+    }
+ })();
