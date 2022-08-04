bdddc9a62499c719e6cc954ffbdd18b39cc4c2c1
Sujin <ikosdu60@gmail.com>
2022-08-04 18:20:59 +0900

    [ADD]JS: 패스트캠퍼스 데이터타입

---
+++ 2022-08-04/datatype.js
@@ -0,0 +1,16 @@
+// 문자열 선언
+// 따옴표 혹은 쌍따옴표를 사용한다.
+let text = 'hello';
+let name = "하이요";
+
+// boolean
+// 참 혹은 거짓을 나타낸다.
+let good = true;
+let loading = false;
+
+// null과 undefined
+// 둘다 의마가 없다는 것을 뜻하지만 역할이 다름.
+//null: 진짜 없다 / undefined: 아직 정해지지 않았다.
+let friend = null;
+let criminal;
+console.log(criminal); // 결과로 undefined가 찍힘.
