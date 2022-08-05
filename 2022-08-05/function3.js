a903f01a92c42a4f0ab973bb786d32e02ebc7912
Sujin <ikosdu60@gmail.com>
2022-08-05 09:20:42 +0900

    [ADD]JS: 패스트캠퍼스 Template Literal

---
+++ 2022-08-05/function3.js
@@ -0,0 +1,9 @@
+//ES6란? ECMAScript6를 의미하며, 자바스크립트의 버전을 가리킨다.
+//2015년에 도입되어 ES2015라고 불리기도 한다.
+//리턴을 하는 순간 함수가 종료된다.
+
+function hello(name){
+    console.log(`Hello ${name}!`);
+}
+
+hello('suri');
