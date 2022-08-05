d63e23fa1b1e2b7e50d6e1417af7320ae9234271
Sujin <ikosdu60@gmail.com>
2022-08-05 15:03:25 +0900

    [ADD]JS: 패스트캠퍼스 객체

---
+++ 2022-08-05/object1.js
@@ -0,0 +1,12 @@
+// 객체: 어떤 값을 선언할 때 하나의 이름에 여러종류의 값을 넣을 수 있게 해준다.
+// 키에는 공백이 있으면 안된다. 공백만들려면 작은따옴표 써야함.
+
+const dog = {
+    name: '멍멍이',
+    age: 2,
+    cute: true,   
+}
+
+console.log(dog);
+console.log(dog.name);
+console.log(dog.age);
