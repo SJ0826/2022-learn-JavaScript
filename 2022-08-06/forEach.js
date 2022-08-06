1a31531d0bcb3a12e9daa3c538ca244083af9ca3
Sujin <ikosdu60@gmail.com>
2022-08-06 20:52:01 +0900

    [ADD] JS: 패스트캠퍼스 forEach

---
+++ 2022-08-06/forEach.js
@@ -0,0 +1,18 @@
+const superheroes = [
+  '아이언맨',
+  '캡틴 아메리카',
+  '닥터 스트레인지'
+]
+
+function print(hero) {
+  console.log(hero);
+}
+
+superheroes.forEach(print);
+superheroes.forEach(function(hero) {
+  console.log(hero);
+});
+
+superheroes.forEach(hero => {
+  console.log(hero);
+});
