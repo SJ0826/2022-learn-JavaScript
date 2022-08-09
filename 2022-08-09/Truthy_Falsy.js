e1960ca2b6fd29fd48f2fab06e18870a63106e85
Sujin <ikosdu60@gmail.com>
2022-08-09 15:12:23 +0900

    [ADD] JS: 패스트캠퍼스 Truthy와 Falsy

---
+++ 2022-08-09/Truthy_Falsy.js
@@ -0,0 +1,13 @@
+//Truthy & Falsy
+console.log(!undefined); //true JS에서 undefined는 false로 간주한다.
+console.log(!null);
+console.log(0);
+console.log(!' ');
+console.log(!NaN);
+console.log(!false);
+
+console.log(!2);
+console.log(!'hello');
+console.log(!['array']);
+console.log(![]);
+
