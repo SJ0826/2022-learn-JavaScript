3b47f82aedc12939b9d5d815e669345db992c379
Sujin <ikosdu60@gmail.com>
2022-08-09 19:19:08 +0900

    [ADD] JS: 패스트캠퍼스 조건문 활용

--- 2022-08-09/conditional2.js
+++ 2022-08-09/conditional2.js
@@ -4,18 +4,32 @@
 //  if (animal === '참새') return '짹짹';
 //  if (animal === '비둘기') return '구ㅜ구 구ㅜ구 ';
 //}
+// 객체를 활용한 조건문
+// 넣어주는 값에 따라 반환하는 값이 다른 경우 객체를 사용하면 편하다.
 
-function getSound(animal) {
-  const sounds = {
-    개 : '멍멍!',
-    고양이 : '야옹~',
-    참새 : '짹짹',
-    비둘기 : '구구 구 구'
-  };
-  return sounds[animal] || '...?';
+
+function makeSound(animal) {
+  const tasks = {
+    개 : () => {
+      console.log('멍멍!');
+    },
+    고양이() {
+      console.log('야옹!');
+    },
+    비둘기() {
+      console.log('구구 구구');
+    }
+  }
+
+  const task = tasks[animal];
+  if (!tasks) {
+    console.log('...?');
+    return;
+  }
+  console.log(task);
 }
 
-console.log(getSound('개'));
-console.log(getSound('고양이'));
-console.log(getSound('참새'));
-console.log(getSound('비둘기'));
+
+makeSound('개');
+makeSound('비둘기');
+makeSound('인간');
