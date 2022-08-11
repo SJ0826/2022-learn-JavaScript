d2ae2192b984cd95c990ff7cdd4789b2b09443b8
Sujin <ikosdu60@gmail.com>
2022-08-11 14:48:30 +0900

    [ADD] JS: 패스트캠퍼스 promise

--- 2022-08-11/async3.js
+++ 2022-08-11/async3.js
@@ -1,11 +1,23 @@
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(new Error());
-  }, 1000);
-});
+function increaseAndPrint(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const value = n + 1;
+      if ( value === 5 ) {
+        const error = new Error();
+        error.name = 'ValueIsFiveError';
+        reject(error);
+        return;
+      }
+      console.log(value);
+      resolve(value);
+    }, 1000);
+  });
+}
 
-myPromise.then(result => {
-  console.log(result);
-}).catch(e => {
-  console.error(e);
-})
+increaseAndPrint(0).then(increaseAndPrint)
+.then(increaseAndPrint)
+.then(increaseAndPrint)
+.then(increaseAndPrint)
+.then(increaseAndPrint)
+
+// 단점: 어느 부분에서 에러가 발생했는지 파악하기 힘들다.
