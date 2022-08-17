3b89d7cbf1ef2b7c6157ad51b7868dd3ab1c13f2
Sujin <ikosdu60@gmail.com>
2022-08-17 14:03:41 +0900

    [ADD] JS: 드림코딩 리팩토링

---
+++ 2022-08-16/src/popup.js
@@ -0,0 +1,26 @@
+'use stric';
+
+export default class PopUp { // 외부에서 클래스를 받을 수 있음.
+  constructor() {
+  this.popUp = document.querySelector('.pop-up');
+  this.popUpText = document.querySelector('.pop-up__message');
+  this.popUpRefresh = document.querySelector('.pop-up__refresh');
+  this.popUpRefresh.addEventListener('click', () => {
+    this.onClick && this.onClick();
+    this.hide();
+  })
+  }
+  setClickListener(onClick) {
+    this.onClick = onClick;
+  }
+
+  showWithText(text) {
+    this.popUpText.innerText = text;
+    this.popUp.classList.remove('pop-up--hide');
+  }
+
+  hide() {
+    this.popUp.classList.add('pop-up--hide');
+  }
+}
+
