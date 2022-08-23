d8ad7e2bb2b68e92a84bf694d8916e013482a727
Sujin <ikosdu60@gmail.com>
2022-08-23 09:16:25 +0900

    [ADD] JS: 코어 자바스크립트 prototype과 __proto__

---
+++ 2022-08-23/example1.js
@@ -0,0 +1,9 @@
+var Constructor = function (name) {
+  this.name = name;
+};
+Constructor.prototype.method1 = function() {};
+Constructor.prototype.property1 = 'Constructor Prototype Property';
+
+var instance = new Constructor('Instance');
+console.log(Constructor);
+console.log(instance);
