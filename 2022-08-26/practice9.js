877961b5df5c4a15ef40abe83b52048b4b47b7c5
Sujin <ikosdu60@gmail.com>
2022-08-26 09:03:25 +0900

    [ADD] JS: 모던 자바스크립트 튜토리얼 setInterval

---
+++ 2022-08-26/practice9.js
@@ -0,0 +1,14 @@
+// setInterval
+function printNumber(from, to) {
+  let current = from;
+
+  let timerId = setInterval(function() {
+    console.log(current);
+    if (current == to) {
+      clearInterval(timerId);
+    }
+    current++;
+  }, 1000);
+}
+
+printNumber(5, 10);
