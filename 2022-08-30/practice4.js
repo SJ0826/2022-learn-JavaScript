0bc72d7e35ad09bfade124ef5b3ba37c54540de3
Sujin <ikosdu60@gmail.com>
2022-08-30 18:17:17 +0900

    [ADD] JS: 모던 자바스크립트 메서드 오버라이딩

---
+++ 2022-08-30/practice4.js
@@ -0,0 +1,30 @@
+class Animal {
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+  run(speed) {
+    this.speed = speed;
+    alert(`${this.name} 은/는 속도 ${this.speed}로 달립니다.`);
+  }
+  stop() {
+    this.speed = 0;
+    alert(`${this.name} 이/가 멈췄습니다.`);
+  }
+}
+
+class Rabbit extends Animal {
+  hide() {
+    alert(`${this.name} 이/가 숨었습니다!`);
+  }
+
+  stop() {
+    super.stop();
+    this.hide();
+  }
+}
+
+let rabbit = new Rabbit("흰 토끼");
+
+rabbit.run(5); 
+rabbit.hide()
