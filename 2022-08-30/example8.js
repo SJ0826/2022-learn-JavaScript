5e833816c1b679211f713f0326a6f62a85af04a9
Sujin <ikosdu60@gmail.com>
2022-08-30 16:22:03 +0900

    [ADD] JS: 코어 자바스크립트 다중 프로토타입 체이닝

---
+++ 2022-08-30/example8.js
@@ -0,0 +1,15 @@
+var Grade = function () {
+  var args = Array.prototype.slice.call(arguments);
+  for (var i = 0; i < args.length; i++) {
+    this[i] = args[i];
+  }
+  this.length = args.length;
+};
+var g = new Grade(100, 80);
+
+Grade.prototype = [];
+
+console.log(g);
+g.pop();
+console.log(90);
+console.log(g);
