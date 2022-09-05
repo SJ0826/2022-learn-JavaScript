91bac0bf0650077de053d3535d2d9afde5daa3af
Sujin <ikosdu60@gmail.com>
2022-09-05 15:30:31 +0900

    [ADD] JS: 모던 자바스크립트 튜토리얼 프로토타입 상속

---
+++ 2022-09-05/practice9.js
@@ -0,0 +1,20 @@
+let animal = {
+  jumps: null
+};
+let rabbit = {
+  __proto__: animal,
+  jumps: true
+};
+
+console.log( rabbit.jumps );
+
+delete rabbit.jumps;
+
+console.log( rabbit.jumps );
+
+delete animal.jumps;
+
+console.log( rabbit.jumps );
+
+
+
