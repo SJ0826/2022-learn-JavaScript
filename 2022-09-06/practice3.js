2fd4a9a2fd7ea5213f77e5a7790c5477d58d5df0
Sujin <ikosdu60@gmail.com>
2022-09-06 15:58:29 +0900

    [ADD] JS: 모던 자바스크립트 튜토리얼 콜백

---
+++ 2022-09-06/practice3.js
@@ -0,0 +1,11 @@
+function loadScript(src, callback) {
+  let script = document.createElement('script');
+  script.src = src;
+  script.onload = () => callback(script);
+  document.head.append(script);
+}
+
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
+  console.log(`&{script.src}가 로드되었습니다.`);
+  console.log(_);
+});
