5569b3db50d656fc3ae9d2ed87f7a2e7851145f5
Sujin <ikosdu60@gmail.com>
2022-09-07 17:47:46 +0900

    [ADD] JS: 드림코딩 리액트

--- 2022-09-07/example1.js
+++ 2022-09-07/example1.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
 class LikeButton extends Component {
+  /* state */
   state = {
     numnerOfLikes: 0,
   };
+  /* render */
   render() {
     return <button>
       {this.state.numberOfLikes}
