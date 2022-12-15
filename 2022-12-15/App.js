13fa23408e807f2e219e3147d702a009619b1f7d
sujin <ikosdu60@gmail.com>
2022-12-15 17:09:36 +0900

    [ADD] JS: 블랙커피 빈 값에 입력에 대한 반응 | 예외처리

---
+++ 2022-12-15/App.js
@@ -0,0 +1,30 @@
+const $ = (selector) => document.querySelector(selector);
+
+function App() {
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventdefault();
+  });
+
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key !== "Enter") {
+      return;
+    }
+    if ($("#espresso-menu-name").value === "") {
+      alert("값을 입력해주세요.");
+      return;
+    }
+    if (e.key === "Enter") {
+      const espressoMenuName = $("#espresso-menu-form").value;
+      const menuItemTemplate = (espressoMenuName) => {};
+      $("#espresso-menu-list").insertAdjacentHTML(
+        "beforeend",
+        menuItemTemplate(espressoMenuName)
+      );
+      const memuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+      $(".menu-count").innerText = `총 ${memuCount}개`;
+      $("#espresso-menu-name").value = "";
+    }
+  });
+}
+
+App();
